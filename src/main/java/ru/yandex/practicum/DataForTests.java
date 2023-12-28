750b0be180205d866ebe9c61a7eeb3e5348b39de
Nistel <duz3000@gmail.com>
2023-12-28 20:40:33 +0300

    test : add tests create, login and update user

--- src/main/java/ru/yandex/practicum/DataForTests.java
+++ src/main/java/ru/yandex/practicum/DataForTests.java
@@ -1,12 +1,78 @@
 package ru.yandex.practicum;
 
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import ru.yandex.practicum.user.User;
+
+import static io.restassured.RestAssured.given;
+
 public class DataForTests {
     public static final String BASE_URI = "https://stellarburgers.nomoreparties.site";
-    public static final String URI_FOR_ORDERS = "/api/orders";
-    public static final String URI_FOR_REGISTER_USER = "/api/auth/register";
-    public static final String URI_FOR_LOGIN_USER = "/api/auth/login";
-    public static final String URI_FOR_UPDATE_OR_DELETE_USER = "/api/auth/user";
-    public static final String URI_FOR_LOGOUT_USER = "/api/auth/logout";
-    public static final String URI_FOR_UPDATE_TOKEN = "/api/auth/token";
-    public static final String URI_FOR_GET_INGREDIENTS = "/api/auth/ingredients";
+    private static final String URI_FOR_ORDERS = "/api/orders";
+    private static final String URI_FOR_REGISTER_USER = "/api/auth/register";
+    private static final String URI_FOR_LOGIN_USER = "/api/auth/login";
+    private static final String URI_FOR_UPDATE_OR_DELETE_USER = "/api/auth/user";
+    private static final String URI_FOR_LOGOUT_USER = "/api/auth/logout";
+    private static final String URI_FOR_UPDATE_TOKEN = "/api/auth/token";
+    private static final String URI_FOR_GET_INGREDIENTS = "/api/auth/ingredients";
+    public static String accessToken;
+    public static String refreshToken;
+
+    @Step("Create basic user for use in tests")
+    public static Response createUserInApi(User user) {
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .and()
+                .body(user)
+                .when()
+                .post(URI_FOR_REGISTER_USER);
+        accessToken = response.then().extract().path("accessToken");
+        refreshToken = response.then().extract().path("refreshToken");
+
+        return response;
+    }
+
+    @Step("Create basic user for use in tests")
+    public static Response loginUserInApi(User user) {
+        return given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", accessToken)
+                .and()
+                .body(user)
+                .when()
+                .post(URI_FOR_LOGIN_USER);
+    }
+
+    @Step("Create basic user for use in tests")
+    public static Response updateDataUserInApi(User user) {
+        return given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", accessToken)
+                .and()
+                .body(user)
+                .when()
+                .patch(URI_FOR_UPDATE_OR_DELETE_USER);
+    }
+
+    @Step("Create basic user for use in tests")
+    public static Response updateDataUserWithoutAutorizInApi(User user) {
+        return given()
+                .header("Content-Type", "application/json")
+                .and()
+                .body(user)
+                .when()
+                .patch(URI_FOR_UPDATE_OR_DELETE_USER);
+    }
+
+    @Step("Create basic user for use in tests")
+    public static Response deleteUser() {
+        if (accessToken != null){
+            return given()
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", accessToken)
+                    .when()
+                    .delete(URI_FOR_UPDATE_OR_DELETE_USER);
+        }
+       return null;
+    }
 }
--- src/test/java/ru/yandex/practicum/CreatingUserTest.java
+++ src/test/java/ru/yandex/practicum/CreatingUserTest.java
@@ -1,11 +1,61 @@
 package ru.yandex.practicum;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import ru.yandex.practicum.user.User;
+import ru.yandex.practicum.user.UserGenerator;
+
+import static org.apache.http.HttpStatus.SC_ACCEPTED;
+import static org.apache.http.HttpStatus.SC_FORBIDDEN;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.matchesRegex;
 
 public class CreatingUserTest {
+    private User user;
+
     @Before
     public void setUp() {
         RestAssured.baseURI = DataForTests.BASE_URI;
+        user = UserGenerator.randomUser();
+    }
+
+    @Test
+    public void testCreateUserCorrectDataReturnTokenAnd200() {
+        Response response = DataForTests.createUserInApi(user);
+
+        response.then()
+                .statusCode(SC_OK)
+                .assertThat().body("accessToken", matchesRegex("^Bearer .*"));
+    }
+
+    @Test
+    public void testCreateUserAlreadyRegisteredDataReturnErrorAnd403() {
+        DataForTests.createUserInApi(user);
+        String tokenForDeleteUser = DataForTests.accessToken;
+        Response response = DataForTests.createUserInApi(user);
+        DataForTests.accessToken = tokenForDeleteUser;
+
+        response.then()
+                .statusCode(SC_FORBIDDEN)
+                .assertThat().body("message", matchesRegex("User already exists"));
+    }
+
+    @Test
+    public void testCreateUserNoPartDataReturnErrorAnd403() {
+        user.withPassword("");
+        Response response = DataForTests.createUserInApi(user);
+
+        response.then()
+                .statusCode(SC_FORBIDDEN)
+                .assertThat().body("message", matchesRegex("Email, password and name are required fields"));
+    }
+
+    @After
+    public void setDown() {
+        DataForTests.deleteUser();
     }
 }
